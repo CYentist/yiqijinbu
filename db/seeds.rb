ebd3ade19c752caab58a55a414e5987f13ae714b
seewhymoon <seewhymoon@gmail.com>
2017-04-25 22:09:12 +0800

    seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts '建立一个账号'
+u = User.new
+u.email = 'admin@test.com'
+u.password = '123456'
+u.password_confirmation = '123456'
+u.is_admin = true
+u.save
+
+puts '建立40个course'
+
+Course_demo = [["title1","description1"],["title2","description2"],["title3","description3"],["title4","description4"],["title5","description5"]]
+
+create_courses = for i in 1..40 do
+courses_test=Course_demo[rand(0..4)]
+Course.create!([title:courses_test[0],description:courses_test[1]])
+end
